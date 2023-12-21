9356db8a79376767320d3f44ee1196c2c75af4db
Alex <110344381+axgw@users.noreply.github.com>
2023-12-21 01:26:37 +0100

    Update: character to value mapping

--- ascii_art.py
+++ ascii_art.py
@@ -11,11 +11,17 @@ def get_pixel_matrix(img_file: str):
 
 
 def get_brightness_matrix(pixel_matrix: []):
-    matrix_brightness = [[0 for _ in i] for i in pixel_matrix]
+    brightness_matrix = [[0 for _ in i] for i in pixel_matrix]
     for x, row in enumerate(pixel_matrix):
         for y, pixel in enumerate(row):
-            matrix_brightness[x][y] = (round((pixel[0] + pixel[1] + pixel[2]) / 3.0))
-    return matrix_brightness
+            brightness_matrix[x][y] = (round((pixel[0] + pixel[1] + pixel[2]) / 3.0))
+    return brightness_matrix
 
 
+def char_val_mapping(brightness_matrix: [], ascii_chars: []):
+    ascii_matrix = [[0 for _ in i] for i in brightness_matrix]
+    for x, row in enumerate(brightness_matrix):
+        for y, pixel in enumerate(row):
+            ascii_matrix[x][y] = ascii_chars[int(pixel / 255 * len(ascii_chars) - 1)]
+    return ascii_matrix
 
